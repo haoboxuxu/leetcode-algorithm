80d248176b94748fd2ce8100c5ab92909ed4f196
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2019-05-29 23:10:23 +0800

    Create 4_MedianofTwoSortedArrays.java

---
+++ 4_MedianofTwoSortedArrays.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length, left = (m + n + 1) / 2, right = (m + n + 2) / 2;
+        return (findKth(nums1, nums2, left) + findKth(nums1, nums2, right)) / 2.0;
+    }
+    int findKth(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length, n = nums2.length;
+        if (m == 0) return nums2[k - 1];
+        if (n == 0) return nums1[k - 1];
+        if (k == 1) return Math.min(nums1[0], nums2[0]);
+        int i = Math.min(m, k / 2), j = Math.min(n, k / 2);
+        if (nums1[i - 1] > nums2[j - 1]) {
+            return findKth(nums1, Arrays.copyOfRange(nums2, j, n), k - j);
+        } else {
+            return findKth(Arrays.copyOfRange(nums1, i, m), nums2, k - i);
+        }
+    }
+}
