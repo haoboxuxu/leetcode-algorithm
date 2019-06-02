9591d4e8557d289b1113db6dc4fa76ea9601737b
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2019-06-02 09:39:07 +0800

    Create 13_RomantoInteger.java

---
+++ 13_RomantoInteger.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+        if(s == null || s.length() == 0) return 0;
+        int res = toNum(s.charAt(0));
+        
+        for(int i = 1; i < s.length(); i++) {
+        	if(toNum(s.charAt(i)) > toNum(s.charAt(i-1))) {
+        		res += toNum(s.charAt(i)) - 2*toNum(s.charAt(i-1));
+        	}else {
+				res += toNum(s.charAt(i));
+			}
+        }
+        
+        return res;
+    }
+    
+    public static int toNum(char c) {
+    	int res = 0;
+    	switch (c) {
+		case 'I': return 1;
+		case 'V': return 5;
+		case 'X': return 10;
+		case 'L': return 50;
+		case 'C': return 100;
+		case 'D': return 500;
+		case 'M': return 1000;
+		default: break;
+		}
+    	return res;
+    }
+}
