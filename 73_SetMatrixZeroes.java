6ace1d4ce558c6fe69ed312255103e64fee1d57a
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2019-07-10 22:48:10 +0800

    Update 73_SetMatrixZeroes.java

--- 73_SetMatrixZeroes.java
+++ 73_SetMatrixZeroes.java
@@ -35,3 +35,58 @@ class Solution {
     }
 }
 */
+public void setZeroes(int[][] matrix) {
+        int noZeroRawIndex = -1;
+        int rawLength = matrix.length;
+        int colLength = matrix[0].length;
+        for (int i = 0; i < rawLength; i++) {
+            int index = 0;
+            for (index = 0; index < colLength; index++) {
+                if (matrix[i][index] == 0) {
+                    break;
+                }
+            }
+            if (index == colLength) {
+                noZeroRawIndex = i;
+                break;
+            }
+        }
+
+        if (noZeroRawIndex == -1) {
+            for (int i = 0; i < rawLength; i++) {
+                for (int j = 0; j < colLength; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        } else {
+            for (int i = 0; i < rawLength; i++) {
+                for (int j = 0; j < colLength; j++) {
+                    if (matrix[i][j] == 0) {
+                        matrix[noZeroRawIndex][j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 0; i < rawLength; i++) {
+                for (int j = 0; j < colLength; j++) {
+                    if (i == noZeroRawIndex) {
+                        continue;
+                    }
+                    if (matrix[i][j] == 0) {
+                        for (int j2 = 0; j2 < colLength; j2++) {
+                            matrix[i][j2] = 0;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < colLength; i++) {
+                if (matrix[noZeroRawIndex][i] == 0) {
+                    for (int j = 0; j < rawLength; j++) {
+                        matrix[j][i] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
