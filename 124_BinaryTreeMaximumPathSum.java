08f26f42abdf77a6b733ed531d92cc174243eded
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-01-22 22:52:31 +0800

    Update 124_BinaryTreeMaximumPathSum.java

--- 124_BinaryTreeMaximumPathSum.java
+++ 124_BinaryTreeMaximumPathSum.java
@@ -16,23 +16,23 @@ class Solution {
     }
     
     public int maxRootSum(TreeNode root) {
-		if(root == null) {
-			return 0;
-		}
-		int curVal = root.val;
-		int leftMaxSum = maxRootSum(root.left);
-		int rightMaxSum = maxRootSum(root.right);
-		
-		if(leftMaxSum > 0) {
-			curVal += leftMaxSum;
-		}
-		if(rightMaxSum > 0) {
-			curVal += rightMaxSum;
-		}
-		if(curVal > res) {
-			res = curVal;
-		}
-        // return res; !错误理解题目，wa了很多次
-		return Math.max(root.val, Math.max(root.val + leftMaxSum, root.val + rightMaxSum));
+	if(root == null) {
+	    return 0;
 	}
+	int curVal = root.val;
+	int leftMaxSum = maxRootSum(root.left);
+	int rightMaxSum = maxRootSum(root.right);
+		
+	if(leftMaxSum > 0) {
+	    curVal += leftMaxSum;
+	}
+	if(rightMaxSum > 0) {
+	    curVal += rightMaxSum;
+	}
+	if(curVal > res) {
+	    res = curVal;
+	}
+        // return res; !错误理解题目，wa了很多次
+	return Math.max(root.val, Math.max(root.val + leftMaxSum, root.val + rightMaxSum));
+    }
 }
