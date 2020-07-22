d6fa42ee6f779321cf76c429003d059eb0c7b17e
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-07-22 23:47:42 +0800

    Update 112_PathSum.java

--- 112_PathSum.java
+++ 112_PathSum.java
@@ -4,9 +4,28 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == sum && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum- root.val);
+    }
+}
+
+/*
 class Solution {
 	public boolean hasPathSum(TreeNode root, int sum) {
 		if(root == null) {
@@ -25,3 +44,4 @@ class Solution {
 		return false;
 	}
 }
+*/
