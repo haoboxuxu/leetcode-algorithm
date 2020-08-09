d82745f5075433de844928df71c264af51ad99b1
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-08-09 16:48:02 +0800

    Update 206_ReverseLinkedList.java

--- 206_ReverseLinkedList.java
+++ 206_ReverseLinkedList.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//循环（开空间）
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode dummy = null;
@@ -17,6 +18,31 @@ class Solution {
             dummy = temp;
             head = head.next;
         }
+        return dummy;
+    }
+}
+//循环（不开空间）
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode dummy = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = dummy;
+            dummy = cur;
+            cur = next;
+        }
+        return dummy;
+    }
+}
+//递归
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
         return dummy;
     }
 }
