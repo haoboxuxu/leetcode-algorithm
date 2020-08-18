b6c178f5fe105a6c2de7fd807a5baba966704a35
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-08-18 16:49:34 +0800

    Update 46_Permutations.java

--- 46_Permutations.java
+++ 46_Permutations.java
@@ -1,31 +1,29 @@
 class Solution {
-    public static List<List<Integer>> permute(int[] nums) {
-    	List<List<Integer>> res = new ArrayList<List<Integer>>();
-    	 dfs(res, nums, 0);
-    	 return res;
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(res, nums, 0);
+        return res;
     }
-    public static void dfs(List<List<Integer>> res, int[] nums, int index) {
-    	if(index == nums.length) {
-    		List<Integer> temp = new ArrayList<Integer>();
-    		for(int i : nums) {
-    			temp.add(i);
-    		}
-    		res.add(temp);
-    	}else {
-    		for(int i = index; i < nums.length; i++) {
-    			swap(nums, index, i);
-    			dfs(res, nums, index + 1);
-    			swap(nums, index, i);
-    		}
-    	}
+    
+    private void dfs(List<List<Integer>> res, int[] nums, int index) {
+        if (index == nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int n : nums) {
+                temp.add(n);
+            }
+            res.add(temp);
+        }else {
+            for (int i = index; i < nums.length; i++) {
+                swap(nums, index, i);
+                dfs(res, nums, index + 1);
+                swap(nums, index, i);
+            }
+        }
     }
-    public static void swap(int a[], int i, int j) {
-    	int temp = a[i];
-    	a[i] = a[j];
-    	a[j] = temp; 
+    
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    public static void main(String[] args) {
-    	int[] nums = {1, 2, 3};
-    	System.out.println(permute(nums));
-	}
 }
