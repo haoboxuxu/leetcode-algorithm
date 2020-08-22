c8e3dd4c23bce2a1d400d97719281adbf7ebff8e
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-08-22 19:24:47 +0800

    Update 79_WordSearch.java

--- 79_WordSearch.java
+++ 79_WordSearch.java
@@ -1,8 +1,12 @@
 class Solution {
+    int n, m;
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (word.charAt(0) == board[i][j] && bfs(i, j, 0, board, word)) {
+        n = board.length;
+        m = board[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word.charAt(0) && bfs(i, j, 0, board, word)) {
                     return true;
                 }
             }
@@ -10,26 +14,26 @@ class Solution {
         return false;
     }
 
-    private boolean check(int i, int j, int count, char[][] board, String word) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(count)) {
+    private boolean check(int i, int j, int index, char[][] board, String word) {
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word.charAt(index)) {
             return false;
         }
         return true;
     }
 
-    private boolean bfs(int i, int j, int count, char[][] board, String word) {
-        if (count == word.length()) {
+    private boolean bfs(int i, int j, int index, char[][] board, String word) {
+        if (index == word.length()) {
             return true;
         }
-        if (!check(i, j, count, board, word)) {
+        if (!check(i, j, index, board, word)) {
             return false;
         }
         char temp = board[i][j];
         board[i][j] = ' ';
-       boolean res = bfs(i-1, j, count+1, board, word) ||
-                     bfs(i+1, j, count+1, board, word) ||
-                     bfs(i, j+1, count+1, board, word) ||
-                     bfs(i, j-1, count+1, board, word);
+        boolean res = bfs(i-1, j, index+1, board, word) ||
+                      bfs(i+1, j, index+1, board, word) ||
+                      bfs(i, j+1, index+1, board, word) ||
+                      bfs(i, j-1, index+1, board, word);
         board[i][j] = temp;
         return res;
     }
