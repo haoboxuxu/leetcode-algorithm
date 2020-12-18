6fd55f672d2a467d7dea045c8b7208372d860ffa
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-12-18 20:47:17 +0800

    Create 62_UniquePaths.java

---
+++ 62_UniquePaths.java
@@ -0,0 +1,31 @@
+//n memory
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) dp[i] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j-1];
+            }
+        }
+
+        return dp[n-1];
+    }
+}
+//n^2 memory
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int j = 0; j < m; j++) dp[j][0] = 1;
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+}
