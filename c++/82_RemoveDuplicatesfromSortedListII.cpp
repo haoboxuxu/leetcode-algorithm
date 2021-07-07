1abd37af2b757c55fd00237c8c079419a747da4e
haoboxuxu <haoboxuxu@icloud.com>
2021-07-07 14:32:30 +0800

    updates

---
+++ c++/82_RemoveDuplicatesfromSortedListII.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) return head;
+        
+        ListNode* dummy = new ListNode(0, head);
+        
+        ListNode* cur = dummy;
+        
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                int x = cur->next->val;
+                while (cur->next && cur->next->val == x) {
+                    cur->next = cur->next->next;
+                }
+            } else {
+                cur = cur->next;
+            }
+        }
+        
+        return dummy->next;
+    }
+};
